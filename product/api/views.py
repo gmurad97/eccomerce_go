cce86266dbb3aa7d17b5fdbc3a51d72a7b353a99
Murad Gazymagomedov <murad.dev@bk.ru>
2025-06-05 23:59:09 +0400

    cbv + paginator

--- product/api/views.py
+++ product/api/views.py
@@ -3,6 +3,13 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from product.api.serializers import ProductListSerializer
 from product.models import Product
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.pagination import (
+    PageNumberPagination,
+)
+from ..models import Product
+from .serializers import ProductListSerializer
 
 
 @api_view(["GET"])
@@ -11,3 +18,14 @@ def index(request: HttpRequest):
     serializer = ProductListSerializer(product, many=True)
     print(serializer.data)
     return Response(serializer.data)
+
+
+class ProductListAPIView(APIView):
+
+    def get(self, request):
+        products = Product.objects.all().order_by("id")
+
+        paginator = self.pagination_class()
+        page = paginator.paginate_queryset(products, request)
+        serializer = ProductSerializer(page, many=True)
+        return paginator.get_paginated_response(serializer.data)
